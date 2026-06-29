e0c57114424111343ae49c0476bc33f67ad84198
CMQTRADE <sheiladiazcmq@gmail.com>
2026-06-29 11:41:05 -0300

    Add general configuration in config.js

---
+++ js/config.js
@@ -0,0 +1,33 @@
+/* ==========================================
+   CONFIGURACIÓN GENERAL
+========================================== */
+
+const CONFIG = {
+
+    OBJETIVO: 0.46,
+
+    BUSINESS_BEYOND: [
+        "MATCH",
+        "AGUAS",
+        "UNG"
+    ],
+
+    COLUMNAS: {
+
+        PROMOTOR: "PROMOTOR",
+
+        DISTRIBUIDOR: "desc_ddc_wh",
+
+        NEGOCIO: "BUSINESS",
+
+        TAREA: "TAREA",
+
+        SUPERVISOR: "supervisor",
+
+        ENVIADAS: "CANTIDAD_TAREAS",
+
+        VALIDADAS: "TAREAS_VALIDADAS"
+
+    }
+
+};
